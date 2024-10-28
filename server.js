63ba4278c35e90fd1522bfd216f1fefd51843516
nyasatech <bmtonga@unima.ac.mw>
2024-10-28 08:45:56 +0200

    fix

--- server.js
+++ server.js
@@ -93,8 +93,8 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(bodyParser.json());
-app.use('/images', express.static('upload/images'));
-app.use('/uploads', express.static('uploads'));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/upload', express.static(path.join(__dirname, 'upload/images')));
 
 
 // JWT Authentication Middleware
